a490fd71b80d821816bbbdb04d73bd32b64e0fb8
Simone Gentili <sensorario@gmail.com>
2025-08-28 20:32:48 +0200

    feat(help): add missing shortcut

--- src/components/HelpModal.tsx
+++ src/components/HelpModal.tsx
@@ -6,6 +6,7 @@ export const HelpModal = ({ setShowHelp }) => ((
         >
             <h2>Scorciatoie</h2>
             <ul style={{ marginTop: '1rem', marginBottom: '1rem' }}>
+                <li><strong>Ctrl+H</strong>: Mostra questa finestra</li>
                 <li><strong>Ctrl+N</strong>: Aggiungi un nuovo task</li>
                 <li><strong>Ctrl+X</strong>: Cancella tutti i task skippati o completati</li>
                 <li><strong>Click su task</strong>: Cambia stato del task</li>
