c5c289e1a69c89fdee8d10672de17e905f7eb1e6
Simone Gentili <sensorario@gmail.com>
2025-08-29 10:07:45 +0200

    feat(task): make urls, links

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,17 +1,29 @@
 import { STATUS } from "../utils";
 
-export const TaskList = ({ tasks, onTaskClick }) => (
-    <ul className="task-list">
-        {tasks.map(task => (
-            <li
-                key={task.id}
-                className="task-item"
-                onClick={() => onTaskClick(task.id)}
-            >
-                <strong>{STATUS[task.status]}</strong> - {task.title}
-            </li>
-        ))}
-    </ul>
-);
+export const TaskList = ({ tasks, onTaskClick }) => {
+
+    const handler = (task) => {
+        let title = task.title;
+
+        // if title contains a link
+        const urlRegex = /(https?:\/\/[^\s]+)/g;
+        const hasLink = urlRegex.test(title);
+        if (hasLink) {
+            title = title.replace(urlRegex, (url) => {
+                return `<a href="${url}" class="task-link" target="_blank" rel="noopener noreferrer">${url}</a>`;
+            });
+        }
+
+        return <li key={task.id} className="task-item" onClick={() => onTaskClick(task.id)}>
+            <strong>{STATUS[task.status]}</strong> - <span dangerouslySetInnerHTML={{ __html: title }} />
+        </li>
+    };
+
+    return (
+        <ul className="task-list">
+            {tasks.map(handler)}
+        </ul>
+    );
+}
 
 export default TaskList;
