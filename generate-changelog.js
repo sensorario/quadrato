820ed595167f619527e82f7ea2f2c7d697402ab0
Simone Gentili <sensorario@gmail.com>
2025-09-20 00:47:28 +0200

    Update changelog

--- generate-changelog.js
+++ generate-changelog.js
@@ -22,8 +22,11 @@ function getGitLogByTag() {
             if (/Merge (branch|pull request|remote-tracking branch|.*)/i.test(line)) continue;
             const match = line.match(/^(\w+) (\d{4}-\d{2}-\d{2}) (.+)$/);
             if (match) {
-                const [, hash, date, message] = match;
-                const hashLink = `[#${hash}]`;
+                let [, hash, date, message] = match;
+                // Replace (close #123) or (#123) with markdown link to GitHub issue
+                message = message.replace(/\(close #(\d+)\)/gi, (_, p1) => `(close [#${p1}](https://github.com/sensorario/quadrato/issues/${p1}))`);
+                message = message.replace(/#(\d+)/g, (m, p1) => `[#${p1}](https://github.com/sensorario/quadrato/issues/${p1})`);
+                const hashLink = `[${hash}](https://github.com/sensorario/quadrato/commit/${hash})`;
                 if (/^(feat|feature|add|implement)/i.test(message)) {
                     features.push(`- ${message} (${hashLink}, ${date})`);
                 } else if (/^(fix|bug|hotfix|patch|resolve)/i.test(message)) {
