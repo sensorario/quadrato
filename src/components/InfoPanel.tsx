7408b7f724a067cefab8df8f542c4ce5f2094929
Simone Gentili <sensorario@gmail.com>
2025-09-23 06:19:04 +0200

    feat(info): leave margin between tabs and info (close #18)

--- src/components/InfoPanel.tsx
+++ src/components/InfoPanel.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 
 export const InfoPanel = () => {
-    return <>
+    return <div style={{ fontSize: '12px', color: '#666', textAlign: 'center', marginTop: '16px' }}>
         <strong>Versione:</strong> v1.1
         <br />
         <strong>Creato da:</strong> sensorario
         <br />
         <strong>Repository:</strong> <a href="https://github.com/sensorario/quadrato">sensorario/quadrato</a>
-    </>
+    </div>
 };
 
 export default InfoPanel;
