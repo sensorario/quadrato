f41ad7b06480046fb22bf6ff97ea860dcac48b3d
Simone Gentili <sensorario@gmail.com>
2026-01-06 11:37:18 +0100

    fix(modal-footer): add missing margin in modal footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export const Footer = ({ children }: { children: React.ReactNode }) => {
-    return <div style={{ display: 'flex', gap: '1rem', justifyContent: 'right' }}>
+    return <div style={{ marginTop: '1rem', display: 'flex', gap: '1rem', justifyContent: 'right' }}>
         {children}
     </div>;
 }
