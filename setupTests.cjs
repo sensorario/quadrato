e01b597ceaa703d8c15fc266db9d72890bb07bf0
Simone Gentili <sensorario@gmail.com>
2026-01-19 06:45:45 +0100

    chore(test): hide console during test execution

--- setupTests.cjs
+++ setupTests.cjs
@@ -1,1 +1,10 @@
 require('@testing-library/jest-dom');
+
+if (process.env.NODE_ENV === 'test') {
+    global.console = {
+        ...console,
+        log: jest.fn(),
+        debug: jest.fn(),
+        info: jest.fn(),
+    };
+}
